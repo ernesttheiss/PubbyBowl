86aa8cad349f46c8914d175e775544a8982e33ed
ernesttheiss <theissernest@ymail.com>
2023-12-06 16:57:54 -0800

    Block23-4

--- puppybowl.js
+++ puppybowl.js
@@ -1,43 +1,44 @@
-const cohortName = '2310-fsa-et-web-pt-sf';
-
-;// Define the base API endpoint
-const apiEndpoint = 'https://fsa-puppy-bowl.herokuapp.com/api/${2310-fsa-et-web-pt-sf}/';
-
 // DOM elements
-const app = document.getElementById('app');
-const rosterContainer = document.getElementById('roster-container');
-const detailsContainer = document.getElementById('details-container');
-const addPlayerForm = document.getElementById('add-player-form');
-const puppyRainContainer = document.getElementById('puppy-rain-container');
+let playerContainer = document.getElementById('all-players-container');
+let newPlayerFormContainer = document.getElementById('new-player-form');
+let puppyListDiv = null; // The image and info of the puppy will be inside this div
 
-// Function to fetch and render the roster
-const fetchRoster = async () => {
-  try {
-    // Fetch roster data from the API
-    const response = await fetch(`${apiEndpoint}/roster`);
-    const roster = await response.json();
+// Cohort information
+const cohortName = '2310-FSA-ET-WEB-PT-SF';
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
-    // Render the roster
-    rosterContainer.innerHTML = `
-      <h2>Roster</h2>
-      <div class="player-list">
-        ${roster.map(renderPlayer).join('')}
-      </div>`;
-  } catch (error) {
-    console.error('Error fetching roster:', error);
-  }
+// State object
+let state = {
+    allPlayers: [],
+    singlePlayer: null, // This is where the info of the player selected will be stored
+    newPup: {},
 };
 
+// Fetch all players from the API
+const fetchAllPlayers = async () => {
+    try {
+        let response = await fetch(APIURL + "players");
+        let playersData = await response.json();
+        state.allPlayers = playersData.data.players;
+        return playersData.data.players;
+    } catch (err) {
+        console.error('Uh oh, trouble fetching players!', err);
+    }
+};
+
+// Show more details when the "Show More!" button is clicked
 async function showHide(idx) {
-    document.querySelector(`#span${idx} button`).style.display = "none"; // Fix: Hide the button, not the span
+    document.querySelector(`#span${idx}`).style.display = "none";
     await fetchSinglePlayer(idx);
-};
+}
 
+// Hide extra information when the "Show Less" button is clicked
 function removeExInfo(id) {
     document.querySelector(`#extraInfo${id}`).style.display = "none";
     document.querySelector(`#span${id}`).style.display = "block";
 }
 
+// Fetch information of a single player
 const fetchSinglePlayer = async (playerId) => {
     try {
         const response = await fetch(APIURL + "players/" + playerId);
@@ -46,7 +47,7 @@ const fetchSinglePlayer = async (playerId) => {
         let exInfoDiv = document.querySelector(`#extraInfo${playerId}`);
         exInfoDiv.style.display = "block";
         exInfoDiv.innerHTML = `
-            <p>Status: ${state.singlePlayer.status}</p>     
+            <p>Status: ${state.singlePlayer.status}</p>
             <p>Team ID: ${state.singlePlayer.teamId}</p>
             <button onclick="removeExInfo(${state.singlePlayer.id})">Show Less</button>
             <button onclick="removePlayer(${state.singlePlayer.id})">Remove</button>
@@ -56,14 +57,19 @@ const fetchSinglePlayer = async (playerId) => {
     }
 };
 
-const addNewPlayer = async (playerObj) => {
+// Add a new player
+const addNewPlayer = async (e) => {
+    e.preventDefault();
     try {
-        const response = await fetch(APIURL + "players/", {
+        const response = await fetch(APIURL + "players", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(playerObj),
+            body: JSON.stringify({
+                name: document.getElementById("puppyName").value,
+                breed: document.getElementById("puppyBreed").value,
+            }),
         });
         const result = await response.json();
         console.log(result);
@@ -73,6 +79,7 @@ const addNewPlayer = async (playerObj) => {
     }
 };
 
+// Remove a player
 const removePlayer = async (playerId) => {
     try {
         const response = await fetch(APIURL + "players/" + playerId, {
@@ -89,24 +96,26 @@ const removePlayer = async (playerId) => {
     }
 };
 
+// Render all players to the DOM
 const renderAllPlayers = (playerList) => {
     try {
         let renderedPlayers = playerList.map((p) => {
             let indPlayer = document.createElement("ul");
             indPlayer.innerHTML = `
-            <div class="card">
-                <img src=${p.imageUrl} alt="${p.breed} puppy">
-                <div class="container${p.id} mainPuppyInfo">
-                    <h4>Name: ${p.name}</h4>
-                    <p>Breed: ${p.breed}</p>
-                    <div id="extraInfo${p.id}" style="display:none">
+                <div class="card">
+                    <img src=${p.imageUrl} alt="${p.breed} puppy">
+                    <div class="container${p.id} mainPuppyInfo">
+                        <h4>Name: ${p.name}</h4>
+                        <p>Breed: ${p.breed}</p>
+                        <div id="extraInfo${p.id}" style="display:none"></div>
+                        <div>
+                            <span id="span${p.id}">
+                                <button onclick="showHide(${p.id})"> Show more!</button>
+                                <button onclick="removePlayer(${p.id})">Remove</button>
+                            </span>
+                        </div>
                     </div>
-                <div>
-                <span id="span${p.id}">
-                <button onclick="showHide(${p.id})"> Show more!</button>
-                <button onclick="removePlayer(${p.id})">Remove</button>
-                </span>
-            </div>
+                </div>
             `;
             return indPlayer;
         });
@@ -116,45 +125,40 @@ const renderAllPlayers = (playerList) => {
     }
 };
 
+// Render the new player form to the DOM
 const renderNewPlayerForm = () => {
-    const formHTML = `
-        <h2>Puppy Bowl New Player Form</h2>
-        <form id="addPlayerForm">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-
-            <label for="breed">Breed:</label>
-            <input type="text" id="breed" name="breed" required>
-
-            <label for="imageUrl">Image URL:</label>
-            <input type="url" id="imageUrl" name="imageUrl" required>
-
-            <button type="submit">Add Player</button>
-        </form>
-    `;
-    newPlayerFormContainer.innerHTML = formHTML;
-
-    const addPlayerForm = document.getElementById('addPlayerForm');
-    addPlayerForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(addPlayerForm);
-        const playerObj = {
-            name: formData.get('name'),
-            breed: formData.get('breed'),
-            imageUrl: formData.get('imageUrl'),
-        };
-        addNewPlayer(playerObj);
-    });
+    try {
+        let firstDiv = document.querySelector("#div1Heading");
+        firstDiv.innerHTML = `
+            <h1 id="mainHeading">Puppy Bowl 2023</h1>
+            <div id=formDiv>
+                <h2 id=div1Heading>Puppy Bowl New Player Form</h2>
+                <form id=addPup>
+                    <label>Puppy Name</label><br>
+                    <input type="text" id="puppyName"><br>
+                    <label>Puppy Breed</label><br>
+                    <input type="text" id="puppyBreed"><br>
+                    <button id="addBtn">Add Player</button>
+                </form>
+            </div>
+        `;
+        let addPup = document.querySelector("#addPup");
+        addPup.addEventListener("submit", addNewPlayer);
+    } catch (err) {
+        console.error('Uh oh, trouble rendering the new player form!', err);
+    }
 };
 
+// Clear the DOM and initialize the application
 function clearDom() {
     document.body.removeChild(playerContainer);
     document.body.removeChild(newPlayerFormContainer);
     const newApp = document.createElement("div");
     const newApp2 = document.createElement("div");
     newApp.innerHTML = `<h2 id=div1Heading>Puppy Bowl New Player Form</h2>`;
-    newApp2.innerHTML = `<h2 id=div2Heading>Puppy Bowl Players:</h2>      
-                        <div id=puppyListDiv></div>
+    newApp2.innerHTML = `
+        <h2 id=div2Heading>Puppy Bowl Players:</h2>      
+        <div id=puppyListDiv></div>
     `;
     newApp.id = "new-Player-Container";
     newApp2.id = "all-Player-Container";
@@ -163,8 +167,9 @@ function clearDom() {
     document.body.append(newPlayerFormContainer);
     document.body.append(playerContainer);
     puppyListDiv = document.querySelector("#puppyListDiv");
-};
+}
 
+// Initialize the application
 const init = async () => {
     clearDom();
     const players = await fetchAllPlayers();
@@ -173,6 +178,8 @@ const init = async () => {
 };
 
 init();
+
+
 
 
 
